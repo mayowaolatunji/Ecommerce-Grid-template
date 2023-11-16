dce7bd42acfb65351f86c98d33aad44bbbb864ff
Mayowa Olatunji <olatunjimayowa0396@gmail.com>
2023-11-16 16:52:09 +0100

    Cart Toggle function

---
+++ script.js
@@ -0,0 +1,12 @@
+let main = document.querySelector(".main");
+let cart = document.querySelector(".cart")
+
+function toggleCart(){
+    if (cart.innerHTML.style.display === none){
+        main.innerHTML.style.width === 100%
+    } else{
+        main.innerHTML.style.width === 60%
+    }
+}
+
+
